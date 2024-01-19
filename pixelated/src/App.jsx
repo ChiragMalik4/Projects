f99e34aa8299830da8af28c329f2a9df5f56b834
Chirag Malik <chiragmalik400@gmail.com>
2024-01-19 21:30:41 +0530

    New Project!

--- pixelated/src/App.jsx
+++ pixelated/src/App.jsx
@@ -1,43 +1,21 @@
-import React, { useState } from 'react'
-import Sidebar from './Sidebar.jsx'
-import './Container.css'
+import React from "react";
+import {HashRouter as Router} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
+import Container from "./Container.jsx";
+
+const App =()=>{
+    return(
+        <Router>
+          <Routes>
+            <Route path="/" element={<Container />} />
+          </Routes>
+            
+        </Router>
+       
 
 
-export const App = () => {
-  const [Theme, setTheme] = useState(1)
-  let theme = `theme${Theme}`
-  const Themeapply=()=>{
-    setTheme(Theme+1)
-    if(Theme===4){
-      setTheme(1)
-    }
-  }
-  return (
-    <div>
-      <div className={`container ${theme}`}>
-        
-      </div>
+    )
 
-      <Sidebar>
-        <button className='theme-button' onClick={Themeapply}>
-          Click Me!
-        </button>
-        
-        
-      </Sidebar>
-      <audio controls autoPlay>
-          <source src='Sparkle.mp3' type='audio/mp3'/>
-          Your browser doesn't support the audio element.
-      </audio>
-      
-      
-      
-
-
-    </div>
-        
-      
-  )
 }
 
 
---
+++ pixelated/src/Container.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import Sidebar from './Sidebar.jsx'
+import './Container.css'
+
+
+export const Container = () => {
+  const [Theme, setTheme] = useState(1)
+  let theme = `theme${Theme}`
+  const Themeapply=()=>{
+    setTheme(Theme+1)
+    if(Theme===4){
+      setTheme(1)
+    }
+  }
+  return (
+    <div>
+      <div className={`container ${theme}`}>
+        
+      </div>
+
+      <Sidebar>
+        <button className='theme-button' onClick={Themeapply}>
+          Click Me!
+        </button>
+        
+        
+      </Sidebar>
+      <audio controls autoPlay>
+          <source src='Sparkle.mp3' type='audio/mp3'/>
+          Your browser doesn't support the audio element.
+      </audio>
+      
+      
+      
+
+
+    </div>
+        
+      
+  )
+}
+
+
+export default Container
